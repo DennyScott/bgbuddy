794f5f691bc7727cf163384fae7214118fda21d0
Denny Scott <dennyscott301@gmail.com>
2017-03-26 23:14:05 -0500

    Changed AttendingMeetup to stateless component

--- src/BgGroup/Widgets/AttendingMeetup/AttendingMeetup.js
+++ src/BgGroup/Widgets/AttendingMeetup/AttendingMeetup.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { WidgetTitle, WidgetBody, Widget } from '../../Widget/Widget';
 import ImgTextOverlay from '../../Components/ImgTextOverlay';
 
-export default class AttendingMeetup extends Component {
-  static propTypes = {
-    members: React.PropTypes.arrayOf(React.PropTypes.shape({
-      name: React.PropTypes.string.isRequired,
-      image: React.PropTypes.string.isRequired,
-      attending: React.PropTypes.string.isRequired,
-    })).isRequired,
-  }
+const DisplayMembers = members => (
+  members.map(member => <ImgTextOverlay key={member.id} member={member} />)
+)
 
-  render() {
-    return(
+const AttendingMeetup = props => {
+  const displayMembers = DisplayMembers(props.members);
+
+  return(
       <Widget>
         <WidgetTitle title="Attending" />
         <WidgetBody>
           <div className="member-avatars members">
-          { this.props.members.map( (member, i) => {
-            return (
-              <ImgTextOverlay key={i} member={member} />
-            );
-          })}
+            { displayMembers }
         </div>
         </WidgetBody>
       </Widget>
-    );
-  }
+  );
 }
+
+AttendingMeetup.propTypes = {
+  members: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    id: React.PropTypes.number.isRequired,
+    image: React.PropTypes.string.isRequired,
+    attending: React.PropTypes.string.isRequired,
+  })).isRequired,
+}
+
+export default AttendingMeetup;
