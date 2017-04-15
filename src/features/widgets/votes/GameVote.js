3a7931a85cf3ac5c1cf1fa383da7b5d5f8b40f62
Denny Scott <dennyscott301@gmail.com>
2017-04-14 22:06:24 -0500

    Add base game voting

--- src/features/widgets/votes/GameVote.js
+++ src/features/widgets/votes/GameVote.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { WidgetTitle, WidgetBody, Widget } from '../../../components/widget/Widget';
+import { Card, CardImg, CardBlock, CardTitle, CardText, CardDeck, Button } from 'reactstrap';
 
 const SingleGameComponent = props => (
-  <span>{props.game.name}</span>
+  <Card className="single-game-component">
+    <CardImg top width="100%" src={props.game.image} />
+
+    <CardBlock>
+      <CardTitle>{props.game.name}</CardTitle>
+      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+      <Button className="fa fa-thumbs-up"></Button>
+      <Button className="fa fa-thumbs-down"></Button>
+    </CardBlock>
+  </Card>
 )
 
 const GameVote = props => (
   <Widget>
     <WidgetTitle title="Vote: Select your game" />
     <WidgetBody>
+      <CardDeck>
       {props.games.map((game, i) => (
         <SingleGameComponent key={i} game={game} />
       ))}
-      Select a game
+    </CardDeck>
     </WidgetBody>
   </Widget>
 );
